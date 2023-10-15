d215d7fdd2e4448bf26eb9a99f4323f59d324f6a
Abdurazaq333 <irazaqr986@gmail.com>
2023-10-14 23:51:58 -0700

    solve

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import About from "./components/About";
 import ContactUs from "./components/ContactUs";
 import Experience from "./components/Experience";
 import Home from "./components/Home";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/nav/Navbar";
 import Skills from "./components/Skills";
 
 function App() {
